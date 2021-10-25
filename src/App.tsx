cdad19260c06ba19c05db4b8b87a3460270ad9df
raquelnew <raquelnew3@gmail.com>
2021-10-25 18:16:15 -0300

    implementando front-end

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Home from './paginas/home/Home';
+
+function App() {
+  return (
+    <Home />
+  );
+}
+
+export default App;
+
---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+function Home(){
+    return (
+        <>
+        <h1 className="titulo">Home</h1>
+        <img src="https://m.media-amazon.com/images/I/411iLYOnR1L.jpg" alt="Imagem Tela Inicial" />
+        </>
+    );
+}
+
+export default Home;
